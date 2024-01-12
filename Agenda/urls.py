3863ed9b407ed031e59c3ae2c36c0a4843bef0fb
Ruan Victor <97916151+ruanovski@users.noreply.github.com>
2024-01-12 12:49:51 -0300

    Add files via upload

---
+++ Agenda/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path
+from core import views
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('agenda/', views.listar_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
+    
+
+]
